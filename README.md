d9709d951218d3593732ce0d5959835b380256a2
Esraa Abdelhady <esraaabdou94@gmail.com>
2021-03-18 01:52:22 +0000

    file upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+FreeCodeCamp - APIs and Microservices Projects:
+
 # [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+
+preview link: https://file-metadate.herokuapp.com
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
-
-var app = express();
-
+const app = express();
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const file = req.file;
+  res.json({"name": file.originalname, "type": file.mimetype, "size": file.size});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
